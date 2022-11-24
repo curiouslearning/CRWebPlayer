f2d367129df92cfd63e30c7f074fc6ce3d177552
george dzavashvili <dzavashviligeorge@gmail.com>
2022-11-24 15:08:33 +0400

    Change file extensions to .ts

---
+++ Models/Models.ts
@@ -0,0 +1,13 @@
+import { Book } from "../Models/Book";
+import { Page } from "../Models/Page";
+import { TextElement } from "../Models/TextElement";
+import { ImageElement } from "../Models/ImageElement";
+import { AudioElement } from "../Models/AudioElement";
+
+export {
+    Book,
+    Page,
+    TextElement,
+    ImageElement,
+    AudioElement
+}
