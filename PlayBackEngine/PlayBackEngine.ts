c0217d95e3266a8ddf2ad6349d32dedba59f75f1
george dzavashvili <dzavashviligeorge@gmail.com>
2022-11-24 15:09:11 +0400

    Add new PlayBackEngine class

---
+++ PlayBackEngine/PlayBackEngine.ts
@@ -0,0 +1,37 @@
+// Class that handles the playback of the whole book
+import { Book, Page, TextElement, ImageElement, AudioElement  } from "../Models/Models";
+
+export class PlayBackEngine {
+
+    currentPage: number;
+    numberOfPages: number;
+    transitioningToPage: boolean;
+
+    constructor() {
+        this.currentPage = 0;
+    }
+
+    initializeBook(book: Book) {
+        this.numberOfPages = book.pages.length;
+    }
+
+    goToNextPage() {
+        if (this.transitioningToPage) return;
+        if (this.currentPage < this.numberOfPages) {
+            this.currentPage++;
+        }
+        this.transitionToPage(this.currentPage);
+    }
+
+    goToPreviousPage() {
+        if (this.transitioningToPage) return;
+        if (this.currentPage > 0) {
+            this.currentPage--;
+        }
+        this.transitionToPage(this.currentPage);
+    }
+
+    transitionToPage(pageNumber: number) {
+        this.transitioningToPage = true;
+    }
+}
