896fd389d041a59cbce27c9495fddfdbfaba2fb5
george dzavashvili <dzavashviligeorge@gmail.com>
2023-02-13 21:52:58 +0400

    App initialization book parsing

--- App.ts
+++ App.ts
@@ -2,22 +2,40 @@
 import { ContentParser } from "./Parser/ContentParser";
 import { PlayBackEngine } from "./PlayBackEngine/PlayBackEngine";
 import { Book } from "./Models/Models";
-// import Splide from "./third-party/splide-4.1.3/types/types";
 import { Splide } from "@splidejs/splide";
 
  export class App {
 
     contentParser: ContentParser;
     playBackEngine: PlayBackEngine;
+    
+    contentFilePath: string;
+    imagesPath: string;
+    audioPath: string;
 
-    constructor(contentFilePath: string) {
-        // this.contentParser = new ContentParser(contentFilePath);
-        // let book: Book = this.contentParser.parseBook();
-        // this.playBackEngine = new PlayBackEngine();
+    constructor(contentFilePath: string, imagesPath: string, audioPath: string) {
+        this.contentFilePath = contentFilePath;
+        this.imagesPath = imagesPath;
+        this.audioPath = audioPath;
+        this.contentParser = new ContentParser(contentFilePath);
+        this.playBackEngine = new PlayBackEngine(imagesPath, audioPath);
+    }
+
+    async initialize() {
+        let book: Book = await this.contentParser.parseBook();
+
         console.log("App initialized!");
-        new Splide( '.splide' ).mount();
+        console.log(book);
+
+        this.playBackEngine.initializeBook(book);
     }
 
  }
 
- let app: App = new App("content.json");
+ // Passing absolute path to the content file and resource directories
+let app: App = new App("/BookContent/LetsFlyLevel2En/content/content.json",
+    "/BookContent/LetsFlyLevel2En/content/images/",
+    "/BookContent/LetsFlyLevel2En/content/audio/");
+
+// Initialize the app, beginning to read the content file, parsing  and displaying the book
+app.initialize();
