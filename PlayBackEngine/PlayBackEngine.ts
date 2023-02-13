1fef3eb8b52f68d94dcfb40608e0c072af8b7d56
george dzavashvili <dzavashviligeorge@gmail.com>
2023-02-13 21:48:47 +0400

    Add basic playback capacilities to the playback engine

--- PlayBackEngine/PlayBackEngine.ts
+++ PlayBackEngine/PlayBackEngine.ts
@@ -8,12 +8,73 @@ export class PlayBackEngine {
     numberOfPages: number;
     transitioningToPage: boolean;
 
-    constructor() {
+    imagesPath: string;
+    audioPath: string;
+
+    emptyGlowImageTag: string = "empty_glow_image";
+
+    splideHandle: Splide;
+
+    constructor(imagesPath: string, audioPath: string) {
+        this.imagesPath = imagesPath;
+        this.audioPath = audioPath;
         this.currentPage = 0;
+        this.splideHandle = new Splide('.splide').mount();
     }
 
     initializeBook(book: Book) {
         this.numberOfPages = book.pages.length;
+        for (let i = 0; i < book.pages.length; i++) {
+            const slide = document.createElement('li');
+            slide.classList.add('splide__slide');
+
+            // foreach visualelement in page add to slide
+            for (let j = 0; j < book.pages[i].visualElements.length; j++) {
+                let visualElement = book.pages[i].visualElements[j];
+                if (visualElement.type == "text") {
+                    let textElement: TextElement = visualElement;
+                    let textElementDiv = document.createElement('div');
+
+                    textElementDiv.classList.add('cr-text');
+                    textElementDiv.style.position = "absolute";
+                    textElementDiv.style.webkitTextStroke = "thick #303030";
+                    textElementDiv.style.textShadow = "0.1rem 0.15rem 0.1rem #303030";
+                    textElementDiv.style.fontFamily = "Quicksand";
+                    textElementDiv.style.fontWeight = "800";
+                    textElementDiv.style.fontSize = "1.7em";
+                    textElementDiv.style.top = textElement.positionY + "%";
+                    textElementDiv.style.left = textElement.positionX + "%";
+                    textElementDiv.style.width = textElement.width + "%";
+                    textElementDiv.style.height = textElement.height + "%";
+                    textElementDiv.innerHTML = textElement.textContentAsHTML;
+                    slide.appendChild(textElementDiv);
+                } else if (visualElement.type == "image") {
+                    let imageElement: ImageElement = visualElement;
+
+                    if (imageElement.imageSource === this.emptyGlowImageTag) {
+                        continue;
+                    }
+
+                    let imageElementDiv = document.createElement('div');
+
+                    imageElementDiv.classList.add('cr-image');
+                    imageElementDiv.style.position = "absolute";
+                    imageElementDiv.style.top = imageElement.positionY + "%";
+                    imageElementDiv.style.left = imageElement.positionX + "%";
+                    imageElementDiv.style.width = imageElement.width + "%";
+                    imageElementDiv.style.height = imageElement.height + "%";
+
+                    let imageElementImg = document.createElement('img');
+                    imageElementImg.src = this.imagesPath + imageElement.imageSource.replace("images/", "");
+                    imageElementImg.style.width = "100%";
+                    imageElementImg.style.height = "100%";
+                    imageElementDiv.appendChild(imageElementImg);
+                    slide.appendChild(imageElementDiv);
+                }
+
+                this.splideHandle.add(slide);
+            }
+        }
     }
 
     goToNextPage() {
