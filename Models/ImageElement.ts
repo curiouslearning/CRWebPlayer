7216c3a5db348a1c8050e0c3587503badadcbdae
george dzavashvili <dzavashviligeorge@gmail.com>
2023-02-13 21:48:04 +0400

    Update image element

--- Models/ImageElement.ts
+++ Models/ImageElement.ts
@@ -1,10 +1,10 @@
 // ImageElement type that describes the image element model
 
 export type ImageElement = {
+    type: string;
     positionX: number;
     positionY: number;
     width: number;
     height: number;
     imageSource: string;
-    styles: string;
 }
