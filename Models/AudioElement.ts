7b1a5aa2d56b0ab87641a7953872aa60b8aaca93
george dzavashvili <dzavashviligeorge@gmail.com>
2023-02-13 21:47:55 +0400

    Update audio element

--- Models/AudioElement.ts
+++ Models/AudioElement.ts
@@ -1,9 +1,11 @@
 // AudioElement type that describes the audio element model
 
 export type AudioElement = {
+    type: string;
     positionX: number;
     positionY: number;
     width: number;
     height: number;
+    audioSrc: string;
     styles: string;
 }
