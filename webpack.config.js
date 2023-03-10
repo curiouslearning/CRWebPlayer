abaa97f4c69dfe548315bea79ee136619fb3cae1
george dzavashvili <dzavashviligeorge@gmail.com>
2023-03-10 11:31:44 +0400

    Add main html file to the webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './App.ts',
@@ -19,4 +20,11 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Curious Reader',
+      template: 'index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
