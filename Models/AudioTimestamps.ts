eb6f6bb11bd1eb23eb518f8ecd9105b49a3767a2
george dzavashvili <dzavashviligeorge@gmail.com>
2023-05-24 12:15:23 +0400

    Add audio timestamps container

---
+++ Models/AudioTimestamps.ts
@@ -0,0 +1,8 @@
+// WordTimestampElements class that contains all the timestamp audio objects for the page
+
+import { WordTimestampElement } from "./WordTimestampElement";
+
+export type AudioTimestamps = {
+    timestamps: WordTimestampElement[];
+}
+
