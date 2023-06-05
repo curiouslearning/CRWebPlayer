e475f0bd308d55e9ed838be26b52d11165c52661
george dzavashvili <dzavashviligeorge@gmail.com>
2023-06-05 19:57:25 +0400

    Add dom ID to timestamps

--- Models/WordTimestampElement.ts
+++ Models/WordTimestampElement.ts
@@ -1,6 +1,7 @@
 // WordTimestampElement class that contains the word and its timestamp, and an individual audio file
 
 export type WordTimestampElement = {
+    domID: string;
     word: string;
     startTimestamp: number;
     endTimestamp: number;
