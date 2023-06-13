168341c5d65c7509ead66b15d5f94715b08a64c7
george dzavashvili <dzavashviligeorge@gmail.com>
2023-06-13 15:52:25 +0400

    Add current index assignment, and active class assignment

--- PlayBackEngine/PlayBackEngine.ts
+++ PlayBackEngine/PlayBackEngine.ts
@@ -79,11 +79,10 @@ export class PlayBackEngine {
                         let currentTime = audioElementDom.currentTime;
                         for (let j = 0; j < audioElement.audioTimestamps.timestamps.length; j++) {
                             if (currentTime >= audioElement.audioTimestamps.timestamps[j].startTimestamp && currentTime <= audioElement.audioTimestamps.timestamps[j].endTimestamp) {
-                                // let wordTimestampElement: WordTimestampElement = audioElement.audioTimestamps.timestamps[i];
-                                // let wordAudioElement = document.getElementById(wordTimestampElement.domID) as HTMLAudioElement;
-                                // wordAudioElement.play();
-                                let wordElement = document.getElementById(audioElement.domID + '_word_' + j) as HTMLSpanElement;
-                                wordElement.style.color = "#FFFF00";
+                                currentIndex = j;
+                                let wordElement = document.getElementById(audioElement.domID + '_word_' + currentIndex) as HTMLDivElement;
+                                wordElement.classList.add('cr-clickable-word-active');
+                            } 
                             }
                         }
                     }
