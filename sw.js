1a1644837b65db6f8c07f046091b668f68e6cf42
george dzavashvili <dzavashviligeorge@gmail.com>
2023-06-22 20:49:10 +0400

    Builds

--- sw.js
+++ sw.js
@@ -1,18 +1,21 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-routing.dev.js");
-// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-strategies.dev.js");
 
-workbox.precaching.precacheAndRoute([], {});
+workbox.precaching.precacheAndRoute([{"revision":"3fc6d3056ea3eb50d41c0dcc8b97e4b5","url":"dist/app.js"},{"revision":"c3bf00e585782373e1b601c07b513d85","url":"dist/fonts/Quicksand_Bold.otf"},{"revision":"8546bc86df8d5c909a5d0ad5f5ae1291","url":"dist/images/cropped-bird_red-2-up.webp"},{"revision":"891d5740c1af1fad4da3afee1289c11c","url":"dist/images/cropped-bird_red-2.webp"},{"revision":"d6223ad2dfebbfe22e932087e0ec74f0","url":"dist/images/red_bird_256.webp"},{"revision":"6f4089ad2ea19d91fcb2ba6d2f174cc2","url":"dist/index.html"},{"revision":"edb54272a3cbc426804f016027a8703b","url":"dist/styles/app.css"},{"revision":"eccaa30f1115ed90515a3f3020fa48c3","url":"index.html"},{"revision":"91d600ca317cc9985a0fc479c783f8ad","url":"manifest.json"}], {});
 
+const channel = new BroadcastChannel("cr-message-channel");
+
+let version = 0.9;
 let cachingProgress = 0;
 
 self.addEventListener("install", async function (e) {
   self.addEventListener("message", async (event) => {
-    // if (event.data.type === "Registration") {
-    //   if (!!!caches.keys().length) {
-    //     let cacheName = await getCacheName(event.data.value);
-    //   }
-    // }
+    console.log("Registration message received in the service worker ");
+    if (event.data.type === "Registration") {
+      if (!!!caches.keys().length) {
+        cachingProgress = 0;
+        let cacheName = await getCacheName(event.data.value);
+      }
+    }
   });
   self.skipWaiting();
 });
@@ -22,26 +25,52 @@ self.addEventListener("activate", function (event) {
   event.waitUntil(self.clients.claim());
 });
 
-const channel = new BroadcastChannel("my-channel");
-
 channel.addEventListener("message", async function (event) {
-  console.log("Caching request received on the service worker!");
+  console.log("Caching request received in the service worker with data: ");
+  console.log(event.data);
   if (event.data.command === "Cache") {
     cachingProgress = 0;
-    await cacheTheBookJSONAndImages(event.data.data.content);
+    await cacheTheBookJSONAndImages(event.data.data);
   }
 });
 
-function cacheTheBookJSONAndImages(contentFilePath) {
-  console.log("Caching the book JSON and images" + contentFilePath);
-  self.clients.matchAll().then((clients) => {
-      clients.forEach((client) =>
-          client.postMessage({
-              msg: "Loading",
-              data: 100,
-          })
-      );
+function cacheTheBookJSONAndImages(data) {
+  console.log("Caching the book JSON and images");
+  let bookData = data["bookData"];
+  let bookAudioAndImageFiles = [];
+  
+  for (let i = 0; i < bookData["pages"].length; i++) {
+    let page = bookData["pages"][i];
+    for (let j = 0; j < page["visualElements"].length; j++) {
+      let visualElement = page["visualElements"][j];
+      if (visualElement["type"] === "audio") {
+        bookAudioAndImageFiles.push("/BookContent/LetsFlyLevel2En/content/" + visualElement["audioSrc"]);
+        for (let k = 0; k < visualElement["audioTimestamps"]["timestamps"].length; k++) {
+          bookAudioAndImageFiles.push("/BookContent/LetsFlyLevel2En/content/" + visualElement["audioTimestamps"]["timestamps"][k]["audioSrc"]);
+        }
+      } else if (visualElement["type"] === "image" && visualElement["imageSource"] !== "empty_glow_image") {
+        bookAudioAndImageFiles.push("/BookContent/LetsFlyLevel2En/content/" + visualElement["imageSource"]);
+      }
+    }
+  }
+
+  bookAudioAndImageFiles.push(data["contentFile"]);
+
+  console.log("Book audio files: ", bookAudioAndImageFiles);
+
+  caches.open(bookData["bookName"]).then((cache) => {
+    cache.addAll(bookAudioAndImageFiles).catch((error) => {
+      console.log("Error while caching the book JSON", error);
+    });
   });
+  // self.clients.matchAll().then((clients) => {
+  //     clients.forEach((client) =>
+  //         client.postMessage({
+  //             msg: "Loading",
+  //             data: 100,
+  //         })
+  //     );
+  // });
 }
 
 self.addEventListener("fetch", function (event) {
@@ -49,39 +78,12 @@ self.addEventListener("fetch", function (event) {
   if (requestUrl.protocol === 'chrome-extension:') {
     return;
   }
-  console.log("Fetch event for ", event.request.url);
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request, {cache: "no-store"}).then((response) => {
-        const clonedResponse = response.clone();
-        const requestString = event.request.url.toLowerCase();
-        // If the response is valid, clone it and store it in the cache
-        if (response.ok) {
-          if (requestString.indexOf('bookcontent') !== -1) {
-            console.log('Book content request');
-            const segments = requestString.split('/');
-            const index = segments.indexOf('bookcontent');
-            if (index !== -1 && index < segments.length - 1) {
-              const nextSegment = segments[index + 1];
-              caches.open("CR_" + nextSegment).then((cache) => {
-                cache.put(event.request, clonedResponse);
-              });
-            }
-          } else if (requestString.indexOf('bookcontent') === -1) {
-            console.log('Non book content request');
-            caches.open('CRCache').then((cache) => {
-              cache.put(event.request, clonedResponse);
-            });
+      caches.match(event.request).then(function (response) {
+          if (response) {
+            return response;
           }
-        }
-        return response;
+          return fetch(event.request);
       })
-      .catch(function(error) {
-        console.error('Failed to fetch file:', error);
-      });
-    })
   );
 });
