cffc39d2d1a846078fcd954ac95f5c4df856cd19
abrsh-10 <abrhamsisay33@gmail.com>
2026-04-01 16:17:26 +0300

    Add "Peace" Story in Hadiyissa

--- BookContent/audio_compression_script.sh
+++ BookContent/audio_compression_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of books to process to avoid processing every single book in the directory
-process_books=("VeryMuchHadiyissa")
+process_books=("PeaceHadiyissa")
 skipped_books=0
 
 # Initialize variables for tracking progress
