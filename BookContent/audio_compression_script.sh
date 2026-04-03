1b30b567c4cab27a385d1d873079f874bb3097d2
abrsh-10 <abrhamsisay33@gmail.com>
2026-04-03 11:52:22 +0300

    Add "Guess what i am" Story in Kembatissa

--- BookContent/audio_compression_script.sh
+++ BookContent/audio_compression_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of books to process to avoid processing every single book in the directory
-process_books=("TheUmbrellasKembatissa")
+process_books=("GuessWhatIAmKembatissa")
 skipped_books=0
 
 # Initialize variables for tracking progress
