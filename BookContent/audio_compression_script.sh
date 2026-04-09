ff45b010add8d69d6bdd96f74211deb5463bf9fa
Melat16 <melatkassaye13@gmail.com>
2026-04-09 12:43:52 +0300

    add "Very Much" Story in Afaan Oromo

--- BookContent/audio_compression_script.sh
+++ BookContent/audio_compression_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of books to process to avoid processing every single book in the directory
-process_books=("TheLostEggAmharic")
+process_books=("VeryMuchAfaanOromo")
 skipped_books=0
 
 # Initialize variables for tracking progress
