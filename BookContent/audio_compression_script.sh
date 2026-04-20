5f3883b3720643c53ba2cb17fd0eabf9ee301687
Melat16 <melatkassaye13@gmail.com>
2026-04-20 14:27:38 +0300

    add "Peace" Story in Af Somali

--- BookContent/audio_compression_script.sh
+++ BookContent/audio_compression_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of books to process to avoid processing every single book in the directory
-process_books=("ThankYouAnimalsAfSomali")
+process_books=("PeaceAfSomali")
 skipped_books=0
 
 # Initialize variables for tracking progress
