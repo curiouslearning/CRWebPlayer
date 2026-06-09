f160a11ebac2977d6f85449a6efb5b9402f83cd3
Melat16 <melatkassaye13@gmail.com>
2026-06-09 10:28:18 +0300

    added last slide to all the umbrellas stories

--- BookContent/audio_compression_script.sh
+++ BookContent/audio_compression_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of books to process to avoid processing every single book in the directory
-process_books=("ILoveHadiyissa")
+process_books=("TheUmbrellasHadiyissa")
 skipped_books=0
 
 # Initialize variables for tracking progress
