4a8a9ed96fefe0feb37ae87d53d3e736ac5c645d
Melat16 <melatkassaye13@gmail.com>
2026-06-10 14:27:51 +0300

    added last slide to all thank you animals  stories

--- BookContent/audio_compression_script.sh
+++ BookContent/audio_compression_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of books to process to avoid processing every single book in the directory
-process_books=("PeaceAmharic")
+process_books=("ThankYouAnimalsHadiyissa")
 skipped_books=0
 
 # Initialize variables for tracking progress
