93abdde9b0c32f448b7d082760276f9ccb12cf61
abrsh-10 <abrhamsisay33@gmail.com>
2026-06-16 11:34:22 +0300

    fixed peace story for Tembarsa Language

--- BookContent/audio_compression_script.sh
+++ BookContent/audio_compression_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of books to process to avoid processing every single book in the directory
-process_books=("TheLostEggKembatissa")
+process_books=("PeaceTembarsa")
 skipped_books=0
 
 # Initialize variables for tracking progress
